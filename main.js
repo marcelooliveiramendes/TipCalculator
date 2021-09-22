67decc35b51eb77f0de39d3ee5684fbd3d8d61b1
Marcelo Oliveira <marcelooliveiraah09@gmail.com>
2021-09-21 21:09:19 -0300

    JS

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
-let tipValue = document.querySelector("#tip").value;
-
 
 function tipCalc(){
+    
     let tipValue = document.querySelector("#tip").value;
     let valueTip = document.querySelector(".valueTip");
-
+    
     valueTip.innerText = tipValue + "%"; 
+    
+    let total = document.querySelector(".total");
+    let valueBill = document.querySelector("#valueBill").value;
+    
+    let totalBill = parseInt(valueBill) + parseInt(tipValue);
+
+    total.innerText = "$" + totalBill;
+
+
+
+
 }
